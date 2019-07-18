fc6553f6e9cd398dbf872c9b516d6f041e9ca36e
Baekchanhee <chb2561@gmail.com>
2019-07-18 16:09:24 +0900

    ㅄㅅ

--- app.js
+++ app.js
@@ -33,9 +33,11 @@ var port = process.env.PORT || 3000;
 
 app.use(logger('dev', {}));
 app.use(bodyParser.json());
+
 app.use('/api', apiRouter);
 app.use(express.static(__dirname+'/public'));
-
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
