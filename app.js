ed32be5462890fe62a6f17ade9e6f0846aa8cf09
Baekchanhee <chb2561@gmail.com>
2019-07-18 18:33:25 +0900

    length id

--- app.js
+++ app.js
@@ -236,6 +236,8 @@ apiRouter.post('/transaction', function(req, res){
 	var sql = "SELECT * FROM user WHERE kakaoId = ?";
 	
 	var result = connectionsyn.query(sql, [id]);
+	console.log(id);
+	console.log(result.length);
 	if(result.length == 0){
 		var responseBody = {
 			"version": "2.0",
