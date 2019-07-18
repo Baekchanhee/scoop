c9ddd84226e09458531561b02ad9b046e75550a1
hanjieun <hanje0225@gmail.com>
2019-07-18 11:37:07 +0900

    커트라인 sql수정

--- app.js
+++ app.js
@@ -427,18 +427,24 @@ apiRouter.post('/cutlinescore', function(req, res){
  
 	
 	var sql = "SELECT name, score FROM user WHERE kakaoId = ?;";
+	var naming = result[0].name;
+	var score = result[0].score;
+
+	var sql = "SELECT aptname FROM apt WHERE kakaoId = ?;";
+	var aptname = result[0].aptname;
+	
 	
 	connection.query(sql, [id], function(err, result){
 	   console.log(result)
 	   console.log(err)
-	   var naming = result[0].name;
-				 var score = result[0].score;
+	  
 				  
 	   var responseBody = {
 				 version: "2.0",
 				 data: {
 						 "score": score,
-						 "name": naming
+						 "name": naming,
+						 "aptname": aptname
 								 }
  
 		 };
