d2283801464fc1d629ab73260ec5a65d3a8f0e42
hanjieun <hanje0225@gmail.com>
2019-07-18 10:13:25 +0900

    app.js 주석 추가

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(express.static(__dirname+'/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-//서버 test용 
+//서버 test용 한지은
 app.get('/', function(req, res) {
 	res.render('hello');
 });
